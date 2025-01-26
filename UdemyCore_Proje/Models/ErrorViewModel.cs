d6e378e066f9cd7c2902a4327b9ad25a6237adc4
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-26 14:15:11 +0300

    Add project files.

---
+++ UdemyCore_Proje/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UdemyCore_Proje.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
