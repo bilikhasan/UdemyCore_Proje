7c1279fd3aa7a24ea33a91018da7743c7551add8
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-27 15:39:19 +0300

    30.bitti

---
+++ UdemyCore_Proje/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCore_Proje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HeaderPartial()
+        {
+            return View();
+        }
+
+        public IActionResult NavbarPartial()
+        {
+            return View();
+        }
+    }
+}
---
+++ UdemyCore_Proje/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace UdemyCore_Proje.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+    }
+}
