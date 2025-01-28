b957572a2941c563c944c388739d1044a3b0df25
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-28 23:40:35 +0300

    46.bitti

--- UdemyCore_Proje/Controllers/AdminController.cs
+++ UdemyCore_Proje/Controllers/AdminController.cs
@@ -8,5 +8,22 @@ namespace UdemyCore_Proje.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
