dd6b5d79fba6c73967e5a99948004cdcd6ee2422
bilikhasan <fatherlyy@hotmail.com.tr>
2025-01-30 23:43:25 +0300

    71.bitti

---
+++ UdemyCore_Proje/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCore_Proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dasgboard";
+            ViewBag.v1 = "İstatistikler";
+            ViewBag.v1 = "İstatistik Sayısı";
+            return View();
+        }
+    }
+}
