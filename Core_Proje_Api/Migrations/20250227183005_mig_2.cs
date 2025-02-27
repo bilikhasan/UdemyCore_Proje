61b82f1125369b18f7fa1e41c84099e1eb84de08
bilikhasan <fatherlyy@hotmail.com.tr>
2025-02-28 00:28:33 +0300

    222.bitti

---
+++ Core_Proje_Api/Migrations/20250227183005_mig_2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core_Proje_Api.Migrations
+{
+    public partial class mig_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
